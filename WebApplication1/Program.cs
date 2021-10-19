106a34e306fd38a3cb9919851e34f4287872a889
nicha <nicha@DESKTOP-J9CNUSQ>
2021-10-19 09:53:18 +0530

    Add project files.

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File(path: "C:\\Users\\nicha\\source\\repos\\UdemyWebAPI\\WebApplication1"
+                , outputTemplate: "{TimeStamp:yyyy-MM-dd} [{Level:u3}] {Message:lj}{NewLine}{Exception}"
+                , rollingInterval: RollingInterval.Day
+                , restrictedToMinimumLevel: LogEventLevel.Information
+                ).CreateLogger();
+
+            try
+            {
+                Log.Information("App is starting!!!");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Failed to start");
+
+            }
+            finally 
+            {
+                Log.CloseAndFlush();
+            }
+            
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
